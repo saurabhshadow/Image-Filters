31a7d78bb6d263e119eda3796d4bacacb9649ac4
Shikhar Johri <30645315+johri002@users.noreply.github.com>
2018-12-03 21:42:28 +0530

    class made

--- sharpening.py
+++ sharpening.py
@@ -1,26 +1,30 @@
-# Example Python Program for contrast stretching
 import cv2
 import numpy as np
 
-def sharp(image):
-	# Create sharpening kernel
-	kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
-			
-	# applying the sharpening kernel to the input image & displaying it.
-	sharpened = cv2.filter2D(image, -1, kernel)
+class sharpening:
+
+	def __init__(self):
+		pass
 	
-	# Noise reduction
-	sharpened = cv2.bilateralFilter(sharpened, 9, 75, 75) 
-	return sharpened
+	def sharp(self,image):
+		# Create sharpening kernel
+		kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
+
+		# applying the sharpening kernel to the input image & displaying it.
+		sharpened = cv2.filter2D(image, -1, kernel)
+
+		# Noise reduction
+		sharpened = cv2.bilateralFilter(sharpened, 9, 75, 75) 
+		return sharpened
 
 
 # Create an image object
 image = cv2.imread("./car.jpg")
 
+tmp_canvas = sharpening()
+res = tmp_canvas.sharp(image)
+cv2.imwrite('sharped.jpg', res)
 cv2.imshow('original',image)
-
-cv2_image = sharp(image)
-
-cv2.imshow('sharp',cv2_image)
-
+cv2.imshow('sharp',res)
 cv2.waitKey(0)
+cv2.destroyAllWindows()
